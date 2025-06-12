947d8cd9d327a0a689f77380b991af0d3a50b858
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-06-12 15:08:09 +0900

    define a collection

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Typesense\Client;
 
 Route::get('/', function () {
-    return view('welcome');
+    $client = new Client([
+        'api_key' => config('services.typesense.api_key'),
+        'nodes' => [
+            [
+                'host' => config('services.typesense.host'),
+                'port' => config('services.typesense.port'),
+                'protocol' => config('services.typesense.protocol'),
+            ],
+        ],
+        'connection_timeout_seconds' => 2,
+    ]);
+
+    $bookSchema = [
+        'name' => 'books',
+        'fields' => [
+            ['name' => 'title', 'type' => 'string'],
+            ['name' => 'author', 'type' => 'string[]'],
+            ['name' => 'publication_year', 'type' => 'int32'],
+            ['name' => 'ratings_count', 'type' => 'int32'],
+            ['name' => 'average_rating', 'type' => 'float'],
+        ],
+        'default_sorting_field' => 'ratings_count',
+    ];
+
+    $client->collections->create($bookSchema);
+
+    // Health check
+    // curl "typesense:8108/collections/books" -H "X-TYPESENSE-API-KEY: xyz";
+
+    return 'Done';
 });
