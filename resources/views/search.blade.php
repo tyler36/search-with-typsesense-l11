f70ea607af8b8f9c6e2aa35b2468af26db6c7780
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-07-01 11:44:54 +0900

    display facets

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -9,18 +9,36 @@
 </head>
 
 <body class="p-8">
-  <form action="/" method="GET">
-    <input class="border border-gray-300 border-b-0 w-full px-4 py-2" type="search" name="q" id="q" required
-      value="{{ request('q') }}">
-  </form>
+  <div class="grid grid-cols-12 gap-10">
+    <aside class="col-span-3">
+      @foreach ($facets as $facet)
+      <div class="border-gray-300 py-2 border">
+      <h3 class="font-bold border-b border-gray-300 px-4 pb-2">{{ ucwords($facet['name']) }}</h3>
+      <ul class="px-4 py-2">
+      @foreach ($facet['filters'] as $filter)
+      <li>{{ $filter['value'] }}</li>
+      @endforeach
+      </ul>
+      </div>
+  @endforeach
+    </aside>
 
-  <ul class="border border-gray-300 py-2 px-4">
-    @forelse ($results as $key => $result)
-    <li>{!! $result !!}</li>
-  @empty
-    <li>No matching results.</li>
-  @endforelse
-  </ul>
+    <div class="col-span-9">
+      <form action="/" method="GET">
+        <input class="border border-gray-300 border-b-0 w-full px-4 py-2" type="search" name="q" id="q" required
+          value="{{ request('q') }}">
+      </form>
+
+      <ul class="border border-gray-300 py-2 px-4">
+        @forelse ($results as $key => $result)
+      <li>{!! $result !!}</li>
+    @empty
+      <li>No matching results.</li>
+    @endforelse
+      </ul>
+    </div>
+  </div>
+
 </body>
 
 </html>
