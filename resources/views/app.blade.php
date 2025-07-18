e4cd27312456017c54a0b5e9d2e9b576a0264278
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-07-18 11:00:20 +0900

    add TailwindCSS CDN

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,10 +2,11 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     @vite('resources/js/app.js')
     @inertiaHead
   </head>
-  <body>
+  <body class="px-8 pt-6">
     @inertia
   </body>
 </html>
