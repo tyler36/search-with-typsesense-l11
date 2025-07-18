f3b9f1431544554b0917a0c084789f783aad442d
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-07-18 11:13:11 +0900

    add facets panel

--- resources/js/Pages/Search/Index.vue
+++ resources/js/Pages/Search/Index.vue
@@ -28,28 +28,43 @@ const searchClient = adapter.searchClient;
 </script>
 
 <template>
-  <div class="">
-    <h1>Search</h1>
+  <div class="max-w-5xl mx-auto">
     <ais-instant-search :search-client="searchClient" index-name="books" class="space-y-2">
-      <ais-search-box/>
-      <ais-stats
-        :class-names="object"
-      />
-      <ais-hits>
-        <template #item="{ item }">
-          <h3 class="font-bold">
-            <ais-highlight attribute="title" :hit="item" />
-          </h3>
-          <div>
-            Author(s): <ais-highlight attribute="authors" :hit="item" />
-          </div>
-          <div>
-            Publish Date: {{ item.publication_year }}
-          </div>
-        </template>
-      </ais-hits>
+      <div class="grid grid-cols-12 gap-8">
+        <aside class="col-span-3">
+          <ais-panel>
+            <template #header>Authors</template>
+            <ais-refinement-list attribute="authors" />
+          </ais-panel>
+        </aside>
+        <div class="col-span-9">
+          <ais-search-box/>
+          <ais-stats
+            :class-names="object"
+          />
+          <ais-hits>
+            <template #item="{ item }">
+              <h3 class="font-bold">
+                <ais-highlight attribute="title" :hit="item" />
+              </h3>
+              <div>
+                Author(s): <ais-highlight attribute="authors" :hit="item" />
+              </div>
+              <div>
+                Publish Date: {{ item.publication_year }}
+              </div>
+            </template>
+          </ais-hits>
+        </div>
+      </div>
     </ais-instant-search>
   </div>
 </template>
 
-<style scoped></style>
+<style>
+.ais-RefinementList-label {
+  display: flex;
+  gap: .5rem;
+  padding: .1rem 0;
+}
+</style>
