3196760f5412c341529b5fb725e52644c943bed3
tyler36 <7234392+tyler36@users.noreply.github.com>
2025-08-20 11:12:12 +0900

    update UI for courses

--- resources/js/Pages/Search/Index.vue
+++ resources/js/Pages/Search/Index.vue
@@ -15,38 +15,23 @@ const adapter = new TypesenseInstantSearchAdapter({
   },
 
   additionalSearchParameters: {
-    query_by: "title",
-    sort_by: "_text_match:desc,ratings_count:desc,publication_year:desc",
+    query_by: "name",
+    // sort_by: "_text_match:desc,ratings_count:desc,publication_year:desc",
   }
 });
 
 const searchClient = adapter.searchClient;
 
-// const search = instantsearch({
-//   searchClient,
-//   indexName: "books"
-// });
-
 </script>
 
 <template>
   <div class="max-w-5xl mx-auto">
-    <ais-instant-search :search-client="searchClient" index-name="books" class="space-y-2">
+    <ais-instant-search :search-client="searchClient" index-name="courses" class="space-y-2">
       <div class="grid grid-cols-12 gap-8">
         <aside class="col-span-full xl:col-span-3 space-y-4">
           <ais-panel>
-            <template #header>Authors</template>
-            <ais-refinement-list attribute="authors" />
-          </ais-panel>
-          <ais-panel>
-            <template #header>Publication Year</template>
-            <ais-numeric-menu attribute="publication_year" :items="[
-                { label: 'All' },
-                { label : '2000 - Now', start: 2000},
-                { label : '1980 - 1999', start: 1980, end: 1989},
-                { label : '1900 - 1979', start: 1900, end: 1979},
-                { label : 'Ancient', end: 1899},
-            ]"></ais-numeric-menu>
+            <template #header>Category</template>
+            <ais-refinement-list attribute="category" />
           </ais-panel>
         </aside>
         <div class="col-span-full xl:col-span-9">
@@ -57,14 +42,8 @@ const searchClient = adapter.searchClient;
           <ais-hits>
             <template #item="{ item }">
               <h3 class="font-bold">
-                <ais-highlight attribute="title" :hit="item" />
+                <ais-highlight attribute="name" :hit="item" />
               </h3>
-              <div>
-                Author(s): <ais-highlight attribute="authors" :hit="item" />
-              </div>
-              <div>
-                Publish Date: {{ item.publication_year }}
-              </div>
             </template>
           </ais-hits>
         </div>
